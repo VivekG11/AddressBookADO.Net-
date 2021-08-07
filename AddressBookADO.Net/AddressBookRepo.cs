8eb5bce870764187bd11c6d18529d7ce7129a35a
VivekG11 <vivekvk2903@gmail.com>
2021-08-07 22:55:23 +0530

    [Displaying details from table ]

--- AddressBookADO.Net/AddressBookRepo.cs
+++ AddressBookADO.Net/AddressBookRepo.cs
@@ -7,8 +7,10 @@ namespace AddressBookADO.Net
 {
     public class AddressBookRepo
     {
+        //creating object for AddressBoookdata class
         AddressBookData data = new AddressBookData();
 
+        //establishing connection to server
         public static string connectionString = @"Server = (localdb)\MSSQLLocalDB; Initial Catalog = AddressBook;";
 
         SqlConnection connection = new SqlConnection(connectionString);
@@ -17,35 +19,46 @@ namespace AddressBookADO.Net
         {
             using(this.connection)
             {
+                //Query to retireve details from table
                 string query = @"select *from Addressbooktable;";
 
                 SqlCommand command = new SqlCommand(query, this.connection);
+                //Opening connection
                 this.connection.Open();
                 SqlDataReader reader = command.ExecuteReader();
-
-                if (reader.HasRows)
+                try
                 {
-                    while (reader.Read())
+                    if (reader.HasRows)
                     {
-                        data.FirstName = reader.GetString(0);
-                        data.Lastname = reader.GetString(1);
-                        data.address = reader.GetString(2);
-                        data.city = reader.GetString(3);
-                        data.state = reader.GetString(4);
-                        data.Zip = reader.GetInt64(5);
-                        data.PhoneNumber = reader.GetInt64(6);
-                        data.email = reader.GetString(7);
-                        data.BookName = reader.GetString(8);
-                        data.type = reader.GetString(9);
+                        while (reader.Read())
+                        {
+                            data.FirstName = reader.GetString(0);
+                            data.Lastname = reader.GetString(1);
+                            data.address = reader.GetString(2);
+                            data.city = reader.GetString(3);
+                            data.state = reader.GetString(4);
+                            data.Zip = reader.GetInt64(5);
+                            data.PhoneNumber = reader.GetInt64(6);
+                            data.email = reader.GetString(7);
+                            data.BookName = reader.GetString(8);
+                            data.type = reader.GetString(9);
 
-                        Console.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}", data.FirstName, data.Lastname, data.address, data.city, data.state, data.Zip, data.PhoneNumber, data.email, data.BookName, data.type);
+                            Console.WriteLine("Records in the table are .....");
+                            Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}", data.FirstName, data.Lastname, data.address, data.city, data.state, data.Zip, data.PhoneNumber, data.email, data.BookName, data.type);
 
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No data exists....");
                     }
                 }
-                else
+                catch(Exception ex)
                 {
-                    Console.WriteLine("No data exists....");
+                    Console.WriteLine(ex.Message);
                 }
+
+                //Closing connectio
                     this.connection.Close();
                 }
             }
